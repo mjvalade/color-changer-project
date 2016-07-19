9c25f499c795b03fc206e20ac447d8fe36136303
MaryJane Valade <mjvalade@gmail.com>
2016-07-18 22:48:45 -0600

    it all works

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// var randomButton = document.querySelector('.random-button');
-// var resetButton = document.querySelector('.reset-button');
-var ranColor = colorRandom();
-// var resetColor = bkgdWhite();
-
-function colorRandom() {
-  return 'rgb(' + (Math.floor(Math.random() * 254)) + ',' + (Math.floor(Math.random() * 254)) + ',' + (Math.floor(Math.random() * 254)) + ')';
-}
-
-$('.random-button').on('click', function() {
-  $('body').css({'background-color': 'colorRandom()'});
+$(function() {
+  $('.random-button').on('click', function() {
+    ranColor = 'rgb(' + (Math.floor(Math.random() * 254)) + ',' + (Math.floor(Math.random() * 254)) + ',' + (Math.floor(Math.random() * 254)) + ')';
+    $('body').css('background-color', ranColor);
+      });
 });
-
-
-// $('.list1').on('click', '.read-button', function () {
-//   $(this).parents('.bookmark').toggleClass("red");
-//   getTotalReadCount();
-//   getTotalUnreadCount();
-// });
 
 $('.reset-button').on('click', function() {
   $('body').css({'background-color': 'white'});
