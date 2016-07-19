fe663f28ae18cb55cdc016c57b0ba50b4b87f41d
MaryJane Valade <mjvalade@gmail.com>
2016-07-18 23:02:03 -0600

    refactor script file

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-$(function() {
   $('.random-button').on('click', function() {
     ranColor = 'rgb(' + (Math.floor(Math.random() * 254)) + ',' + (Math.floor(Math.random() * 254)) + ',' + (Math.floor(Math.random() * 254)) + ')';
     $('body').css('background-color', ranColor);
       });
-});
 
 $('.reset-button').on('click', function() {
   $('body').css({'background-color': 'white'});
